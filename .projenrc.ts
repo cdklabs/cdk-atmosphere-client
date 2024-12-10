0f31de9a3c10cac9d778b8ff8c47c9b9a3155585
Polonsky <aws-cdk-dev+cdklabs@amazon.com>
2024-12-10 14:02:59 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,16 @@
+import { cdk } from "projen";
+const project = new cdk.JsiiProject({
+  author: "AWS",
+  authorAddress: "aws-cdk-dev@amazon.com",
+  defaultReleaseBranch: "main",
+  name: "cdk-atmosphere-client",
+  projenrcTs: true,
+  release: false,
+  repositoryUrl: "https://github.com/cdklabs/cdk-atmosphere-client.git",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
